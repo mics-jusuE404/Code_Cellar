8573c2ee78bb389ea8e3b021c8965c6a56dc460a
Alexander Toenges <a.toenges@uni-muenster.de>
2018-02-20 17:07:15 +0100

    Update Filter_Bam_ISIZE.sh
    State than mawk and not standard awk is used

--- Filter_Bam_ISIZE.sh
+++ Filter_Bam_ISIZE.sh
@@ -2,6 +2,7 @@
 
 #### Extract read pairs from a paired-end BAM file that have a ISIZE (column 9) smaller than a used-supplied value.
 #### Usage ----- ./Filter_Bam_ISIZE.sh in.bam 1000 ------ would get pairs with ISIZE smaller 1kb
+#### Note: Script uses mawk (https://invisible-island.net/mawk/mawk.html) which is way faster than standard awk
 
 samtools view -h $1 | \
   mawk -v LEN=$2 '{if ($9 <= LEN && $9 >= -(LEN) && $9 != 0 || $1 ~ /^@/) print $0}' | \
