9469660abb7cae99d12d0df4210eb78d0d147d28
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-05-14 10:20:51 +0200

    Update sort_genomic.sh

--- sort_genomic.sh
+++ sort_genomic.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#### Usage: ./sort_generic in_file > out_file
+#### Usage: ./sort_generic in_file vcf > out_file
 
 ## Uses mawk for increased speed in comparison to standard awk:
 
+FILE=$1
+TYPE=$2
+
 if [[ $TYPE == "vcf" ]]
   then
-  cat $1 | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k2,2n"}'
+  cat $FILE | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k2,2n"}'
   fi
  
 if [[ $TYPE == "gtf" ]]
   then
-  cat $1 | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k4,4n -k5,5n"}'
+  cat $FILE | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k4,4n -k5,5n"}'
   fi
   
 if [[ $TYPE != "vcf" ]] || [[ $TYPE != "gtf" ]]
@@ -19,6 +22,3 @@ if [[ $TYPE != "vcf" ]] || [[ $TYPE != "gtf" ]]
   echo '$2 must be vcf or gtf -- exiting'
   exit 1
   fi
-
-## Can be parallelized on multiple VCF using GNU parallel, e.g:
-## ls *.vcf | parallel "./sort_vcf.sh {} > {.}_sorted.vcf"
