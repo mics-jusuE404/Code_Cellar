a9fa6e0963dfbe034586a5eff10b5d73da5ebe92
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-05-18 18:13:16 +0200

    Delete annotate_GenomicRegions.sh

--- annotate_GenomicRegions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Take a BED file with genomic positions, check for overlaps with a provided gff3 file:
-## Overlap with intron, exon, CDS, UTRs, intergenic,
-## also find the two closest genes and print the distances in bp
-
-## tbc...
