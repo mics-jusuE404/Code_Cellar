8445e9e2a80f3498c8bb56301d344030360b2b7e
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-06-28 22:23:01 +0200

    Create mosdepth2RPKM.sh

---
+++ mosdepth2RPKM.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## Script uses mosdepth, samtools, awk and bedGraphToBigWig to create an RPKM/FPKM-normalized bigwig file from an indexed BAM file.
+
+## Written by Alexander Toenges, a.toenges@uni-muenster.de, IMTB Muenster 2018
