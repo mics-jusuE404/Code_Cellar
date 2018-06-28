d9c82ae838aa5b24255d2193cf0a7f9fbb08ec0a
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-06-28 11:25:53 +0200

    Update ATACseq_BWAmem.sh

--- ATACseq_BWAmem.sh
+++ ATACseq_BWAmem.sh
@@ -3,19 +3,12 @@
 #### Standard ATAC-seq pipeline for low-level processing from fastq to filtered BAM files using BWA mem for alignment
 #### Written by Alexander Toenges (a.toenges@uni-muenster.de) 2017
 #### Assumes to be in same dir as the fastq files and bwa, samtools, sambamba , GNU parallel and bamCoverage (deeptools) in PATH:
-#### ...and yes the filtering after the alignment is probably fartoo bulky, but I prefer to feed BAMs into MACS for peak calling
-#### that really only contain the reads to be used by MACS2 (macs2 callpeak -t BAM --keep-dup=all --nomodel -f BAMPE)
+#### 
 #################################################################################################################################
 #################################################################################################################################
 
-## Choose the genome and set if fastq file are compressed or not
-## ASSEMBLY can be "hg38", "hg19" or "mm10":
-ASSEMBLY="hg38"
-COMPRESSED="yes"
-
-BWA_HG38="/path/to/bwa_idx"
-BWA_HG19="/path/to/bwa_idx"
-BWA_MM10="/path/to/bwa_idx"
+## Path to BWA index:
+TMP_GENOME=
 
 #################################################################################################################################
 #################################################################################################################################
@@ -26,23 +19,6 @@ date
 echo ''
 
 ## Set species and exit if no valid assembly was specified:
-if [[ $ASSEMBLY == "hg38" ]]; then
-  TMP_GENOME=${BWA_HG38}
-  ECHOOUT="hg38"
-  else
-    if [[ $ASSEMBLY == "hg19" ]]; then
-      TMP_GENOME=${BWA_HG19}
-      ECHOOUT="hg19"
-      else
-        if [[ $ASSEMBLY == "MM10" ]]; then
-          TMP_GENOME=${BWA_MM10}
-          ECHOOUT="mm10"
-        else
-          echo '[ERROR]: Neither hg19/hg38/mm10 selected!'
-          exit
-        fi  
-      fi
-    fi  
 
 echo '=> Alignment to' $ECHOOUT
   
@@ -78,9 +54,9 @@ for i in *_1.fastq*
     
   # ALIGNMENT WITH BWA MEM --- OUTPUTFILE IS are called *_raw.bam
   echo '=> Alignment:' $BASENAME
-  bwa mem -v 2 -t 4 ${TMP_GENOME} ${BASENAME}-trimmed-pair1.fastq ${BASENAME}-trimmed-pair2.fastq | \
-    sambamba view -S -f bam -l 0 /dev/stdin | \
-    sambamba sort -m 6G --tmpdir=./ -l 9 -t 3 -o ${BASENAME}_raw.bam /dev/stdin
+  bwa mem -v 2 -t 24 ${TMP_GENOME} ${BASENAME}-trimmed-pair1.fastq ${BASENAME}-trimmed-pair2.fastq | \
+    sambamba view -S -f bam -l 0 -t 4 /dev/stdin | \
+    sambamba sort -m 50G --tmpdir=./ -l 5 -t 8 -o ${BASENAME}_raw.bam /dev/stdin
     samtools flagstat ${BASENAME}_raw.bam > ${BASENAME}_raw.txt
   rm ${BASENAME}*.fastq
   
@@ -92,10 +68,10 @@ for i in *_1.fastq*
   echo '=> Filtering:' $BASENAME
   KEEPCHR='chr1\|chr2\|chr3\|chr4\|chr5\|chr6\|chr7\|chr8\|chr9\|chr10\|chr11\|chr12\|chr13\|chr14\|chr15\|chr16\|chr17\|chr18\|chr19\|chr20\|chr21\|chr22\|chrX'
                          
-  samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -w $KEEPCHR | \
-    xargs samtools view -bhu -f 2 -F 2304 -q 30 -@ 3 -o ${BASENAME}_fi.bam ${BASENAME}_raw.bam
-    samtools index -@ 3 ${BASENAME}_fi.bam
-    samtools flagstat -@ 3 ${BASENAME}_fi.bam > ${BASENAME}_fi.txt
+  samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -v 'chrM' | \
+    xargs samtools view -bh -f 2 -F 2304 -q 30 -@ 3 -o ${BASENAME}_fi.bam ${BASENAME}_raw.bam
+    sambamba index -t 8 ${BASENAME}_fi.bam
+    sambamba flagstat -t 8 ${BASENAME}_fi.bam > ${BASENAME}_fi.txt
   
   ###################################################################################################
   ###################################################################################################
@@ -103,7 +79,7 @@ for i in *_1.fastq*
   # remove duplicates:
   echo '=> Deduplication:' $BASENAME
   sambamba markdup -r -t 4 ${BASENAME}_fi.bam ${BASENAME}_rmdup.bam
-  samtools flagstat -@ 3 ${BASENAME}_rmdup.bam > ${BASENAME}_rmdup.txt
+  sambamba flagstat -t 8 ${BASENAME}_rmdup.bam > ${BASENAME}_rmdup.txt
   echo ''
   done
   
@@ -112,4 +88,4 @@ for i in *_1.fastq*
   
   ## Browser tracks:
   echo '=> Bigwigs:'
-  ls *_rmdup.bam | parallel "bamCoverage --bam {} -bs 1 --normalizeUsingRPKM --numberOfProcessors --outFileName {.}.bigwig"
+  ls *_rmdup.bam | parallel "bamCoverage --bam {} -bs 1 --normalizeUsingRPKM --numberOfProcessors 16 --outFileName {.}.bigwig"
