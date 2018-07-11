3f46ac60e8507b30080c5ec88bd3a684b9eb61b3
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-07-11 22:34:08 +0200

    list chr by tabix

--- Split_VCF_By_Chr.sh
+++ Split_VCF_By_Chr.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-## Assumes uncompressed VCF, extracts entries by chromosomes using tabix and parallel:
-if [[ ${1: -4} != ".vcf" ]]
+## Assumes at least sorted VCF, extracts entries by chromosomes using tabix and parallel:
+if [[ ${1: -3} != ".gz" ]]
   then
-  echo '[ERROR]: Please provide an uncompressed vcf file'
-  exit; fi
+  echo '[INFO]: File does not seem to be compressed -- compressing now'
+  bgzip $1
+  tabix -p vcf ${1}.gz; fi
 
 ## Get chromosomes that are present in the VCF:
-cat $1 | mawk '$1 ~ /^#/ {next} {print $1 | "sort -k1,1 -u"}' > ${1%.vcf}_chrs.txt
-
-## Compress & index file with tabix:
-cat $1 | mawk '$1 ~ /^#/ {print $0;next} {print $0 | "sort -k1,1 -k2,2n"}' | bgzip -c > ${1%.vcf}_sorted.vcf.gz
-tabix -p vcf ${1%.vcf}_sorted.vcf.gz
+tabix ${1}.gz -l > ${1%.vcf.gz}_chr.txt && \
 
 ## Extract files by chr:
-cat ${1%.vcf}_chrs.txt | parallel "tabix -h ${1%.vcf}_sorted.vcf.gz {} > ${1%.vcf}_{}.vcf"
+cat ${1%.vcf.gz}_chr.txt | parallel "tabix -h ${1}.gz {} > ${1%.vcf.gz}_{}.vcf"
 
-## Usage: ./split_vcf_by_chr.sh input.vcf
+## Usage: ./split_vcf_by_chr.sh input.vcf(.gz)
