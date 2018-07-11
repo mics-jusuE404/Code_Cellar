d6864217b5736da6a8967d60367ff58acc62149a
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-07-11 20:18:32 +0200

    avoid bigwig format, use bedGraph.gz

--- Bam2BigWig_CPM.sh
+++ Bam2BigWig_CPM.sh
@@ -17,8 +17,8 @@ mosdepth -t 8 ${1%.bam} $1
 ## 2. Calculate scaling factor:
 SCALE_FACTOR=$(bc <<< "scale=8;1000000/$(samtools idxstats $1 | awk '{SUM+=$3} END {print SUM}')") 
 
-## 3. Normalize:
+## 3. Normalize and write to compressed bedGraph (can be read by IGV):
 mawk -v SF=${SCALE_FACTOR} 'OFS="\t" {print $1, $2, $3, $4*SF}' <(bgzip -c -d -@ 8 ${BAM%.bam}.per-base.bed.gz) | \
-  sort -k1,1 -k2,2n --parallel=8 | \
-  wigToBigWig /dev/stdin $CHROMSIZES ${BAM%.bam}_CPM.bigwig && \
-  rm ${BAM%.bam}.per-base* ${BAM%.bam}*mosdepth* 
+  sort -k1,1 -k2,2n --parallel=8 | bgzip -@ 4 > ${BAM%.bam}_CPM.bedGraph.gz
+rm ${BAM%.bam}.per-base
+rm ${BAM%.bam}.*mosdepth*
