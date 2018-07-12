ec25d0d36ef85c969338d82f3077c1bbe5f08288
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-07-12 12:21:00 +0200

    no intermediate files anymore
    removed mosdepth, introduced sambamba depth and now use bg2bw for stdin(bg) > bw

--- Bam2BigWig_CPM.sh
+++ Bam2BigWig_CPM.sh
@@ -1,26 +1,64 @@
 #!/bin/bash
 
-## Take an indexed BAM file and create a CPM-normalized bigwig, using
-## - mosdepth for a per-base bed.gz (=bedgraph)
-## - mawk for custom to-CPM conversion and
-## wigToBigWig (kentutils) for bigwig conversion, reading from stdin (may take quiet some memory)
-
 ######################################################################################################
+##
+## Take an indexed BAM file and create a CPM-normalized bigwig without intermediate files, using
+## - sambamba depth +  mawk to get the bedgraph,
+## - mawk for custom to-CPM conversion and
+## - bg2bw (git: cancerit/cgpBigWig) to read the bg from stdin and output the bw
+##
 ######################################################################################################
 
 BAM=$1
 CHROMSIZES=$2
 
-## 1. Make the depth file:
-mosdepth -t 8 ${1%.bam} $1
+######################################################################################################
 
-## 2. Calculate scaling factor:
+## Check bam file:
+
+if [[ $# -ne 2 ]] ; then
+    echo 'Usage: ./Bam2BigWig_CPM.sh in.bam chromSizes.txt'
+    exit 1
+fi
+
+DO_EXIT=$(echo 'BAM file looks corrputed -- exiting' && exit 1)
+samtools quickcheck $1 && echo '[INFO]: Processing' $1 || ${DO_EXIT}
+
+## Check if indexed:
+if [[ ! -f ${1}.bai ]]; then
+  echo '[INFO]: BAM file is not indexed -- indexing now:'
+  sambamba index -t 8 $1
+  fi
+
+######################################################################################################
+
+## Functions:
 SCALE_FACTOR=$(bc <<< "scale=8;1000000/$(samtools idxstats $1 | awk '{SUM+=$3} END {print SUM}')") 
 
-## 3. Normalize and write to compressed bedGraph (can be read by IGV):
-mawk -v SF=${SCALE_FACTOR} 'OFS="\t" {print $1, $2, $3, $4*SF}' <(bgzip -c -d -@ 8 ${BAM%.bam}.per-base.bed.gz) | \
-  sort -k1,1 -k2,2n --parallel=8 | \
-  wigToBigWig /dev/stdin $CHROMSIZES ${1%.bam}_CPM.bigwig
+## Depth with sambamba:
+sambamba depth base -t 8 $1 | mawk 'NR > 1, OFS="\t" {print $1, $2, $3}' | \
   
-rm ${BAM%.bam}.per-base*
-rm ${BAM%.bam}.*mosdepth*
+  ## Convert depth to bedgraph (nathanhaigh/depth2bedgraph.awk)
+  mawk 'BEGIN{FS="\t";OFS="\t"}{
+  if(NR>1 && ($1!=prev_seq || $3!=prev_cov || $2>prev_pos+1)){
+    print prev_seq,start,end,prev_cov
+    start = $2-1
+  } else if(NR==1){
+    start = $2-1
+  }
+  end = $2
+  prev_seq = $1
+  prev_pos = $2
+  prev_cov = $3
+  }
+  END{
+    print prev_seq,start,end,prev_cov
+  }' | \
+    
+    ## Normalize:
+    mawk -v SF=${SCALE_FACTOR} 'OFS="\t" {print $1, $2, $3, $4*SF}' | \
+
+      ## write to bw (cancerit/cgpBigWig)
+      bg2bw -i /dev/stdin -c $CHROMSIZES -o ${1%.bam}_CPM.bigwig  
+    
+######################################################################################################              
