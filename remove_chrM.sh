7c496d8aa57f25fb00aee249fad89bc2d2d937fa
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-07-14 21:57:54 +0200

    send file to stdout

--- remove_chrM.sh
+++ remove_chrM.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#### Take a BAM file, e.g. from ATAC-seq and remove reads mapped to chrM
+#### Take a BAM file, e.g. from ATAC-seq and remove reads mapped to chrM, sending the BAM to stdout
 #### Assumes samtools to be in PATH:
 
 if [[ ! -e ${1}.bai ]];
   then
   echo '[INFO]:' $i 'is not indexed. Creating it now'
+  sambamba index -t 8 $1
   fi
-samtools index $1 && echo '[INFO]:' $i 'is now indexed'
 
 echo '[INFO]: Now removing chrM reads'
 samtools idxstats $1 | \
   cut -f 1 | \
   grep -v 'chrM' | \
-  xargs samtools view -hb -o ${1}_exMito.bam $1 | \
+  xargs samtools view -@ 8 -b $1 | \
 
 echo '[INFO]: Finished'
