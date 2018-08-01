49cd08ebfae5c673c637902880be5bc5194592c4
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-08-01 11:19:22 +0200

    Delete HiSAT2-Stringtie-Wrapper.sh

--- HiSAT2-Stringtie-Wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#### Wrapper script to run HISAT2 and StringTie on a cohort of samples, producing the necessary files
-#### to run differential expression analysis of isoforms with ballgown.
-#### Wrapper uses external scripts for Hisat and Stringtie
