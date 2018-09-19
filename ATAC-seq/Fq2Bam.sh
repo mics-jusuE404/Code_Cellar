9b042a8c26750e100199d4f0b1e67aba38cb5ead
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-19 10:06:21 +0200

    Update Fq2Bam.sh

--- ATAC-seq/Fq2Bam.sh
+++ ATAC-seq/Fq2Bam.sh
@@ -20,7 +20,7 @@
 function Fq2Bam {
 
   BASENAME=$1
-  
+ 
   if [[ ! -e ${BASENAME}_1.fastq.gz ]] || [[ ! -e ${BASENAME}_2.fastq.gz ]]; then
     echo '[ERROR] At least one input file is missing -- exiting' && exit 1
     fi
@@ -33,26 +33,52 @@ function Fq2Bam {
   
   ####################################################################################################################################
   
-  echo '#############################################################################################################################'
-  echo '[START]' $BASENAME 'on:' && date && echo ''
+  echo '#############################################################' >> ${BASENAME}.log
+  echo '[START]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log && echo '' >> ${BASENAME}.log
   
-  seqtk mergepe ${BASENAME}_1.fastq.gz ${BASENAME}_2.fastq.gz | \
-    cutadapt -j 4 -a $ADAPTER1 -A $ADAPTER2 --interleaved -m 18 --max-n 0.1 - | \
-    bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -p -t 16 ${BWA_IDX} /dev/stdin | \
-    samtools fixmate -m -@ 2 -O SAM - - | \
-    samblaster --ignoreUnmated | \
-    sambamba view -f bam -S -l 1 -t 4 -o /dev/stdout /dev/stdin | \
-    sambamba sort -m 100G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin
+  seqtk mergepe ${BASENAME}_1.fastq.gz ${BASENAME}_2.fastq.gz 2>> ${BASENAME}.log | \
+    cutadapt -j 4 -a $ADAPTER1 -A $ADAPTER2 --interleaved -m 18 --max-n 0.1 - 2>> ${BASENAME}.log | \
+    bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -p -t 16 ${BWA_IDX} /dev/stdin 2>> ${BASENAME}.log | \
+    samtools fixmate -m -@ 2 -O SAM - - 2>> ${BASENAME}.log | \
+    samblaster --ignoreUnmated 2>> ${BASENAME}.log 2>> ${BASENAME}.log | \
+    sambamba view -f bam -S -l 1 -t 4 -o /dev/stdout /dev/stdin 2>> ${BASENAME}.log | \
+    sambamba sort -m 50G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin 2>> ${BASENAME}.log
+        
+    samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -v 'chrM|_random|chrU|chrEBV|\*' 2>> ${BASENAME}.log | \
+      xargs sambamba view -f bam -t 8 --num-filter=1/1284 --filter='mapping_quality > 19' 2>> ${BASENAME}.log \
+      -o ${BASENAME}_sorted.bam ${BASENAME}_raw.bam 2>> ${BASENAME}.log 
     
-    samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -v 'chrM|_random|chrU|chrEBV|\*' | \
-      xargs sambamba view -f bam -t 8 --num-filter=1/1284 --filter='mapping_quality > 19' \
-      -o ${BASENAME}_sorted.bam ${BASENAME}_raw.bam
+    ls ${BASENAME}*.bam | parallel "sambamba flagstat -t 8 {} > {.}.flagstat 2>> ${BASENAME}.log"
     
-  echo '[END]' $BASENAME 'on:' && date
-  echo '#############################################################################################################################'
+  echo '[END]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log
+  echo '#############################################################' >> ${BASENAME}.log
 }
 
 export -f Fq2Bam
 
+function mtDNA {
+
+  if [[ ! -e ${BASENAME}_raw.bam ]];
+      then
+      echo '[ERROR] Input BAM for mtDNA content is missing -- exit 1' && exit 1
+      fi
+  
+  if [[ ! -e ${BASENAME}_raw.bam.bai ]];
+    then
+    sambamba index -t ${CORES} ${BASENAME}_raw.bam
+    fi
+    
+  mtReads=$(samtools idxstats ${BASENAME}_raw.bam | grep 'chrM' | cut -f 3)
+  totalReads=$(samtools idxstats ${BASENAME}_raw.bam | awk '{SUM += $3} END {print SUM}')
+
+  echo '[mtDNA Content]' $(bc <<< "scale=2;100*$mtReads/$totalReads")'%' > ${BASENAME}_mtDNA.txt
+}
+
+export -f mtDNA
+
+## Alignment:
 ls *_1.fastq.gz | awk -F "_1.fastq.gz" '{print $1}' | \
-  parallel -j 8 "Fq2Bam {}"
+  parallel -j 8 "Fq2Bam {} && mtDNA {}"
+  
+## Bigwigs:
+ls *_sorted.bam | parallel -k -j 8 "bamCoverage -e --normalizeUsing CPM -bs 1 --bam {} -o {.}_CPM.bigwig -p 16"  
