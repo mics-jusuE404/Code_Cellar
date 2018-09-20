a61e01adb3ca9efc9141ec9db0a4136bd6b4ef76
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-20 16:30:04 +0200

    samtools -> sambamba; indexing on the fly

--- Filter_Bam_ISIZE.sh
+++ Filter_Bam_ISIZE.sh
@@ -4,6 +4,6 @@
 #### Usage ----- ./Filter_Bam_ISIZE.sh in.bam 1000 ------ would get pairs with ISIZE smaller 1kb
 #### Note: Script uses mawk (https://invisible-island.net/mawk/mawk.html) which is way faster than standard awk
 
-sambamba view -f sam -h -t 4 $1 | \
+sambamba view -f sam -h -t 2 $1 | \
   mawk -v LEN=$2 '{if ($9 <= LEN && $9 >= -(LEN) && $9 != 0 || $1 ~ /^@/) print $0}' | \
-  sambamba view -f bam -h -o ${1%.bam}_isize${2}.bam -
+  sambamba view -t 2 -S -f bam -h -o ${1%.bam}_isize${2}.bam /dev/stdin
