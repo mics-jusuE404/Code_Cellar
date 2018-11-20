3ae92912d5588d6458d1df9aad6148feb4962ca0
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-11-20 18:44:58 +0100

    Update Fq2Bam.sh

--- ChIP-seq/Fq2Bam.sh
+++ ChIP-seq/Fq2Bam.sh
@@ -2,8 +2,9 @@
 
 ######################################################################################################################################
 
-## ChIP-seq - alignment of singlg-end fastq files to hg38, including adapter trimming without intermediate files
-## Assumes script in same dir as fastqs, bwa - samtools - samblaster - cutadapt in PATH
+## ChIP-seq - alignment of single-end fastq files to hg38 :::
+## Assumes script in same dir as fastqs, bwa - samtools - samblaster - deeptools - fastqc
+## Last update: 20.11.18
 
 ######################################################################################################################################
 
@@ -26,9 +27,6 @@ function Fq2Bam {
     echo '[ERROR] Input file is missing -- exiting' && exit 1
     fi
   
-  ## Truseq adapter:
-  ADAPTER1="AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
-  
   BWA_IDX=/scratch/tmp/a_toen03/Genomes/hg38/bwa_index_noALT_withDecoy/hg38_noALT_withDecoy.fa
   
   ####################################################################################################################################
@@ -36,19 +34,21 @@ function Fq2Bam {
   echo '#############################################################' >> ${BASENAME}.log
   echo '[START]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log && echo '' >> ${BASENAME}.log
   
-  cutadapt -j 4 -a $ADAPTER1 -m 18 --max-n 0.1 ${BASENAME}.fastq.gz 2>> ${BASENAME}.log | \
-    bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -t 16 ${BWA_IDX} /dev/stdin 2>> ${BASENAME}.log | \
+  ## Map BWA mem regardless of read length:
+  bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -t 16 ${BWA_IDX} ${BASENAME}.fastq.gz 2>> ${BASENAME}.log | \
     samblaster --ignoreUnmated 2>> ${BASENAME}.log 2>> ${BASENAME}.log | \
     sambamba view -f bam -S -l 1 -t 4 -o /dev/stdout /dev/stdin 2>> ${BASENAME}.log | \
     sambamba sort -m 4G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin 2>> ${BASENAME}.log
-        
-    samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' 2>> ${BASENAME}.log | \
-      xargs sambamba view -f bam -t 8 --num-filter=0/1284 --filter='mapping_quality > 19' 2>> ${BASENAME}.log \
-      -o ${BASENAME}_sorted.bam ${BASENAME}_raw.bam 2>> ${BASENAME}.log 
+  
+  ## Remove unmapped and duplicated reads (1028):
+  samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' 2>> ${BASENAME}.log | \
+    xargs sambamba view -f bam -t 8 --num-filter=0/1028 --filter='mapping_quality > 19' 2>> ${BASENAME}.log \
+    -o ${BASENAME}_sorted.bam ${BASENAME}_raw.bam 2>> ${BASENAME}.log 
     
-    ls ${BASENAME}*.bam | parallel "sambamba flagstat -t 8 {} > {.}.flagstat 2>> ${BASENAME}.log"
+  ls ${BASENAME}*.bam | parallel "sambamba flagstat -t 8 {} > {.}.flagstat 2>> ${BASENAME}.log"
     
   echo '[END]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log
+  
   echo '#############################################################' >> ${BASENAME}.log
 }
 
@@ -60,3 +60,6 @@ ls *.fastq.gz | awk -F ".fastq.gz" '{print $1}' | \
   
 ## Bigwigs:
 ls *_sorted.bam | parallel -j 4 "bamCoverage -e 400 --normalizeUsing CPM -bs 1 --bam {} -o {.}_CPM.bigwig -p 16"
+
+## Fastqc:
+ls *.bam | parallel "fastqc {}" 
