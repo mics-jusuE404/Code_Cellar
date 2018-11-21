3792b17d4482ad44bbd73ec4f08b5f1099ef4c5c
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-11-21 15:22:32 +0100

    Update Collapse_Replicates.sh

--- ChIP-seq/Collapse_Replicates.sh
+++ ChIP-seq/Collapse_Replicates.sh
@@ -5,14 +5,31 @@
 
 CONDITIONAL_CAT () {
   BASENAME=$1
+  
+  echo '[INFO]: Working on' $BASENAME
+  
   while read p; do
+  
+    ## If the read length is shorter 36bp, baclklist file and do not cat:
     if [[ $(zcat $p | head -n 40000 | mawk '{if(NR%4==2) print length($1)}' | sort -k1,1n -u) < 36 ]]
       then
       echo ${p} >> ${BASENAME}_excludeCatList.txt
       else
+        
+        ## If there is only one file and not multiple, simply rename:
+        if [[ $(ls ${BASENAME}*.fastq.gz | wc -l) == 1 ]]; then
+          mv $(ls ${BASENAME}*.fastq.gz) ${BASENAME}_combined.fastq.gz
+          
+          ## As only one file, go to next iteration:
+          continue
+          fi
+        
+        ## If multiple files per biol-rep. start combining:
         cat ${p} >> ${BASENAME}_combined.fastq.gz
         echo ${p} >> ${BASENAME}_includeCatList.txt
+        
       fi
+      
     done < <(ls ${BASENAME}*.fastq.gz)   
 }; export -f CONDITIONAL_CAT    
 
