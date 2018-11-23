6621791f6d984926f51d2a098583b6e154f260d1
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-11-23 18:31:24 +0100

    Update lowlevel.sh

--- MPRA/lowlevel.sh
+++ MPRA/lowlevel.sh
@@ -39,7 +39,8 @@ function Fq2Bam {
   ADAPTER1="CTGTCTCTTATACACATCT"
   BWA_IDX=/scratch/tmp/a_toen03/Genomes/hg38/bwa_index_noALT_withDecoy/hg38_noALT_withDecoy.fa
   CHROMSIZES="/scratch/tmp/a_toen03/Genomes/hg38/hg38_chromSizes.txt"
-    
+  
+  ## trim adapters, align and sort:
   echo '[START]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log && echo '' >> ${BASENAME}.log
   cutadapt -j 4 -a $ADAPTER1 -m 36 --max-n 0.1 ${BASENAME}.fastq.gz 2>> ${BASENAME}.log | \
     bwa mem -v 2 \
@@ -47,24 +48,30 @@ function Fq2Bam {
     samblaster --ignoreUnmated 2>> ${BASENAME}.log | \
     sambamba view -f bam -S -l 1 -t 4 -o /dev/stdout /dev/stdin 2>> ${BASENAME}.log | \
     sambamba sort -m 2G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin 2>> ${BASENAME}.log
-        
+    
+    ## kick out non-primary chromosomes and unmapped reads
     samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' 2>> ${BASENAME}.log | \
       xargs sambamba view -f bam -t 8 --num-filter=0/4 --filter='mapping_quality > 19' 2>> ${BASENAME}.log \
       -o ${BASENAME}_sortedDup.bam ${BASENAME}_raw.bam 2>> ${BASENAME}.log 
     
+    ## kick out duplicated previously marked by samblaster
     sambamba view -f bam -t 8 --num-filter=/1028 -o ${BASENAME}_sortedDeDup.bam ${BASENAME}_sortedDup.bam 2>> ${BASENAME}.log
     
+    ## flagstat reports
     ls ${BASENAME}*.bam | parallel "sambamba flagstat -t 8 {} > {.}.flagstat 2>> ${BASENAME}.log"
     
   echo '[END]' $BASENAME 'on:' >> ${BASENAME}.log && date >> ${BASENAME}.log
   
 }; export -f Fq2Bam
 
+ls *.fastq.gz | awk -F ".fastq.gz" '{print $1}' | parallel -j 4 "Fq2Bam {}"
+
 ####################################################################################################################################
 ####################################################################################################################################
 
-## Get the percentage of mitochondrial DNA in library:
+## Get the percentage of mitochondrial DNA in library using the samtools idxstats report:
 function mtDNA {
+
   BASENAME=$1 
   mtReads=$(samtools idxstats ${BASENAME}_raw.bam | grep 'chrM' | cut -f 3)
   totalReads=$(samtools idxstats ${BASENAME}_raw.bam | awk '{SUM += $3} END {print SUM}')
@@ -73,7 +80,7 @@ function mtDNA {
  
 }; export -f mtDNA
 
-ls *.fastq.gz | awk -F ".fastq.gz" '{print $1}' | parallel -j 4 "Fq2Bam {} && mtDNA {}"
+ls *.fastq.gz | awk -F ".fastq.gz" '{print $1}' | parallel -j 4 "mtDNA {}"
 
 ####################################################################################################################################
 ####################################################################################################################################
@@ -82,13 +89,13 @@ ls *.fastq.gz | awk -F ".fastq.gz" '{print $1}' | parallel -j 4 "Fq2Bam {} && mt
 ## will crash if one group has no replicates as sambamba expects at least 2 files for merging
 function COMPLEXITY {
   
-  ## Combine using sambamba merge:
+  ## Combine all replicates per cell type into one BAM using sambamba merge:
   find ./ -maxdepth 1 -name "*_rep*_sortedDup.bam" | \
     awk -F "_rep" '{print $1}' | \
     sort -k1,1 -u | \
     parallel -j 4 "sambamba merge -t 16 {}_combined_sortedDup.bam {}*rep*_sortedDup.bam"
     
-  ## Run preseq c_curve:
+  ## Run preseq c_curve to assess library complexity and saturation at given sequencing depth:
   find ./ -maxdepth 1 -name "*_combined_sortedDup.bam" | \
     parallel "preseq c_curve -o {.}_ccurve -s 5e+05 -seed 1 -bam {}"
     
@@ -101,7 +108,7 @@ COMPLEXITY
 
 function PeaksX {
   
-  ## Call peaks on the combined DNA sets per cell type with extsize correspinding to average fragment size,
+  ## Call peaks on the combined DNA sets per cell type with extsize corresponding to average fragment size,
   ## without q-value filtering, as this can be done manually afterwards on $9 of narrowPeak which is -log10(q)
   source activate py27
   find ./ -maxdepth 1 -name "*DNA*_sortedDeDup.bam" | \
