d799ae880b868c72d77eade3278f66c1798b7b26
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-11-28 17:16:08 +0100

    Update salmon_quant.sh

--- RNA-seq/salmon_quant.sh
+++ RNA-seq/salmon_quant.sh
@@ -6,13 +6,19 @@
 #SBATCH --time=48:00:00 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a_toen03@uni-muenster.de
+#SBATCH --job-name=salmon_TXquant
 #SBATCH --output=salmon_quant.log
 #######
 
-## Use Salmon on paired FASTQ files, providing the basename:
+## Use Salmon on FASTQ files, providing the basename:
+IDX="/scratch/tmp/a_toen03/Genomes/hg38/Gencode_v28/gencode.v28.salmonIDX25"
 
-IDX=/scratch/tmp/a_toen03/Genomes/hg38/Gencode_v28/gencode.v28.salmonIDX31
+function SALMON {
+  salmon quant -l A -i $2 -p 8 --validateMappings --seqBias --gcBias -o ${1}_salmonK25 -1 ${1}_1.fastq.gz -2 ${1}_2.fastq.gz
+}; export -f SALMON
 
-## libtype ISR so first strand, stranded, paired-end
-ls *_1.fastq.gz | awk -F "_1" '{print $1}' | \
-  parallel "salmon quant -l A -i $IDX -p 8 --validateMappings --seqBias --gcBias -o {}_salmonQuant_k31 -1 {}_1.fastq.gz -2 {}_2.fastq.gz"
+## Run salmon:
+ls *_1.fastq.gz | awk -F "_1." '{print $1}' | parallel -j 8 "SALMON {} $IDX"
+
+## fastqc
+ls *.fastq.gz | parallel -j 70 "fastqc {}"
