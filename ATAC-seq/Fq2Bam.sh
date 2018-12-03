0edd833c04042ad633938fb6822253aaca4ade65
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-12-03 22:06:25 +0100

    faster flagstat via tee process substitution

--- ATAC-seq/Fq2Bam.sh
+++ ATAC-seq/Fq2Bam.sh
@@ -17,44 +17,33 @@
 #SBATCH --job-name=ATACseq_Align
 #######
 
-function Fq2Bam {
+######################################################################################################################################
+######################################################################################################################################
 
-  BASENAME=$1
- 
-  if [[ ! -e ${BASENAME}_1.fastq.gz ]] || [[ ! -e ${BASENAME}_2.fastq.gz ]]; then
-    echo '[ERROR] At least one input files is missing -- exiting' && exit 1
+function ExitBam {
 
+  (>&2 echo '[ERROR]' $1 'looks suspicious or is empty -- exiting') && exit 1
+  
+}; export -f ExitBam
+
+######################################################################################################################################
+######################################################################################################################################
+
+## Check if BAM is corrupted or incomplete:
+function BamCheck {
+  
+  BASENAME=${1%_*}
+  samtools quickcheck -q $1 && echo '' >/dev/null || ExitBam $1
+  
+  ## Also check if file is not empty:
+  if [[ $(samtools view $1 | head -n 1 | wc -l) < 1 ]]; then
+    ExitBam $BASENAME
     fi
   
-  ## Nextera adapter:
-  ADAPTER1="CTGTCTCTTATACACATCT"
-  ADAPTER2="CTGTCTCTTATACACATCT"
-  
-  BWA_IDX=/scratch/tmp/a_toen03/Genomes/hg38/bwa_index_noALT_withDecoy/hg38_noALT_withDecoy.fa
-  
-  ####################################################################################################################################
-  
-  echo '#############################################################' >> ${BASENAME}.log
-  (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'started on') <(date))
-  
-  seqtk mergepe ${BASENAME}_1.fastq.gz ${BASENAME}_2.fastq.gz | \
-    cutadapt -j 4 -a $ADAPTER1 -A $ADAPTER2 --interleaved -m 18 --max-n 0.1 - | \
-    bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -p -t 16 ${BWA_IDX} /dev/stdin | \
-    samtools fixmate -m -@ 2 -O SAM - - | \
-    samblaster --ignoreUnmated 2>> ${BASENAME}.log | \
-    sambamba view -f bam -S -l 1 -t 4 -o /dev/stdout /dev/stding | \
-    sambamba sort -m 4G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin
-        
-    samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' | \
-      xargs sambamba view -f bam -t 8 --num-filter=1/1284 --filter='mapping_quality > 19' \
-      -o ${BASENAME}_sorted.bam ${BASENAME}_raw.bam 
-    
-    ls ${BASENAME}*.bam | parallel "sambamba flagstat -t 8 {} > {.}.flagstat"
-    
-  (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'ended on') <(date))
-}
+}; export -f BamCheck  
 
-export -f Fq2Bam
+######################################################################################################################################
+######################################################################################################################################
 
 function mtDNA {
 
@@ -72,13 +61,59 @@ function mtDNA {
   totalReads=$(samtools idxstats ${BASENAME}_raw.bam | awk '{SUM += $3} END {print SUM}')
 
   echo '[mtDNA Content]' $(bc <<< "scale=2;100*$mtReads/$totalReads")'%' > ${BASENAME}_mtDNA.txt
+}; export -f mtDNA
+
+######################################################################################################################################
+######################################################################################################################################
+
+function Fq2Bam {
+
+  BASENAME=$1
+  
+  (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'started on') <(date))
+  
+  if [[ ! -e ${BASENAME}_1.fastq.gz ]] || [[ ! -e ${BASENAME}_2.fastq.gz ]]; then
+    echo '[ERROR] At least one input files is missing -- exiting' && exit 1
+    fi
+  
+  ## Nextera adapter:
+  ADAPTER1="CTGTCTCTTATACACATCT"
+  ADAPTER2="CTGTCTCTTATACACATCT"
+  
+  BWA_IDX=/scratch/tmp/a_toen03/Genomes/hg38/bwa_index_noALT_withDecoy/hg38_noALT_withDecoy.fa
+  
+  
+  
+  seqtk mergepe ${BASENAME}_1.fastq.gz ${BASENAME}_2.fastq.gz | \
+    cutadapt -j 4 -a $ADAPTER1 -A $ADAPTER2 --interleaved -m 18 --max-n 0.1 - | \
+    bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -p -t 16 ${BWA_IDX} /dev/stdin | \
+    samtools fixmate -m -@ 2 -O SAM - - | \
+    samblaster --ignoreUnmated 2>> ${BASENAME}.log | \
+    sambamba view -f bam -S -l 0 -t 4 -o /dev/stdout /dev/stdin | \
+      tee >(sambamba flagstat -t 2 /dev/stdin > ${BASENAME}_raw.flagstat) | \
+    sambamba sort -m 4G --tmpdir=./ -l 6 -t 16 -o ${BASENAME}_raw.bam /dev/stdin  
+    
+    BamCheck ${BASENAME}_raw.bam
+    mtDNA ${BASENAME}_raw.bam
+    
+    samtools idxstats ${BASENAME}_raw.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' | \
+      xargs sambamba view -f bam -t 8 --num-filter=1/1284 --filter='mapping_quality > 19' \
+        -o /dev/stdout ${BASENAME}_raw.bam | \
+        tee ${BASENAME}_sorted.bam | \
+      sambamba flagstat -t 4 /dev/stdin > ${BASENAME}_sorted.flagstat 
+     
+  BamCheck ${BASENAME}_sorted.bam
+  
+  (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'ended on') <(date))
 }
 
-export -f mtDNA
+export -f Fq2Bam
 
-## Alignment:
-ls *_1.fastq.gz | awk -F "_1.fastq.gz" '{print $1}' | \
-  parallel -j 4 "Fq2Bam {} 2>> {}.log && mtDNA {}"
+####################################################################################################################################
+####################################################################################################################################
+
+## Run:
+ls *_1.fastq.gz | awk -F "_1.fastq.gz" '{print $1}' | parallel -j 4 "Fq2Bam {} 2>> {}.log"
   
 ## Bigwigs:
 ls *_sorted.bam | parallel -k -j 8 "bamCoverage -e --normalizeUsing CPM -bs 1 --bam {} -o {.}_CPM.bigwig -p 16"  
