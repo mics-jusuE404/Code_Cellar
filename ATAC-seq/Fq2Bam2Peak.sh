11619321d517beaf3fcb45375c901908d82f8ee0
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-12-18 17:17:23 +0100

    Update Fq2Bam2Peak.sh

--- ATAC-seq/Fq2Bam2Peak.sh
+++ ATAC-seq/Fq2Bam2Peak.sh
@@ -13,7 +13,7 @@
 #SBATCH --time=48:00:00 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a_toen03@uni-muenster.de
-#SBATCH --job-name=ATACseq_Align
+#SBATCH --job-name=ATACseq_Fq2Bam
 #######
 
 ######################################################################################################################################
