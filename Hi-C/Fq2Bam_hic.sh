184f756ba6fdc7656235533749bdfd80e460c9af
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-01-24 11:49:29 +0100

    Update Fq2Bam_hic.sh

--- Hi-C/Fq2Bam_hic.sh
+++ Hi-C/Fq2Bam_hic.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-## Alignment of Hi-C data, each mate treated as single-end to avoid insert size issues:
+#######
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=71
+#SBATCH --partition=normal
+#SBATCH --mem=192G
+#SBATCH --time=48:00:00 
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=a_toen03@uni-muenster.de
+#SBATCH --job-name=HiC_lowlevel
+#######
+
+## Alignment of Hi-C data, each mate treated as single-end to avoid insert size issues.
+## Runs two of the below functions in parallel per himem-normal node:
 
 function Fq2Bam {
 
   BASENAME=$1
+  BWA_IDX=/scratch/tmp/a_toen03/Genomes/hg38/bwa_index_noALT_withDecoy/hg38_noALT_withDecoy.fa
   
   (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'started on') <(date))
   
@@ -14,11 +27,11 @@ function Fq2Bam {
 
   mkdir ${BASENAME}_tmpDir
   
-  bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -t 16 ${BWA_IDX} ${BASENAME}.fastq.gz | \
+  bwa mem -v 2 -R '@RG\tID:'${BASENAME}'_ID\tSM:'${BASENAME}'_SM\tPL:Illumina' -t 34 ${BWA_IDX} ${BASENAME}.fastq.gz | \
     samblaster --ignoreUnmated | \
     sambamba view -f bam -S -l 0 -t 4 -o /dev/stdout /dev/stdin | \
-      tee >(samtools flagstat -@ 2 - > ${BASENAME}_raw.flagstat) | \
-    samtools sort -l 5 -T ./${BASENAME}_tmpDir/${BASENAME}_tmp -@ 16 -m 2G | \
+      tee >(samtools flagstat -@ 2 - > ${BASENAME}_sorted.flagstat) | \
+    samtools sort -l 5 -T ./${BASENAME}_tmpDir/${BASENAME}_tmp -@ 10 -m 3G | \
       tee ${BASENAME}_sorted.bam | \
     samtools index -@ 4 - ${BASENAME}_sorted.bam.bai
     
@@ -26,6 +39,8 @@ function Fq2Bam {
     
     BamCheck ${BASENAME}_sorted.bam
     
+    (>&2 paste -d " " <(echo '[INFO]' 'Fq2Bam for' $1 'ended on') <(date))
+    
     }; export -f Fq2Bam
     
-ls *.fastq.gz | awk -F ".fastq" '{print $1}' | parallel -j 4 "Fq2Bam {}"    
+ls *.fastq.gz | awk -F ".fastq" '{print $1}' | parallel -j 2 "Fq2Bam {} 2>> {}.log"    
