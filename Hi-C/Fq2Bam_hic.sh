e6b168385e32d6df431b747676e8507e576886e1
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-01-24 15:14:37 +0100

    Update Fq2Bam_hic.sh

--- Hi-C/Fq2Bam_hic.sh
+++ Hi-C/Fq2Bam_hic.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=72
 #SBATCH --partition=normal
-#SBATCH --mem=180G
+#SBATCH --mem=88G
 #SBATCH --time=160:00:00 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a_toen03@uni-muenster.de
