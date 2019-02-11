61ffe2fdbdea7c1acc707cfea6984e37ab61f559
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-02-11 22:32:16 +0100

    Update ATACseq_lowlevel_v1.sh

--- ATAC-seq/ATACseq_lowlevel_v1.sh
+++ ATAC-seq/ATACseq_lowlevel_v1.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=72
+#SBATCH --partition=normal
+#SBATCH --mem=80G
+#SBATCH --time=48:00:00 
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=a_toen03@uni-muenster.de
+#SBATCH --job-name=ATACseq_Fq2Bam
+
 ######################################################################################################################################
 
 GENOME="mm10"
@@ -10,19 +19,6 @@ MODE="PE"
 ## Script for lowlevel processing of ATAC-seq data:
 ## Don't forget to set appropriate genome version flag!
 
-######################################################################################################################################
-
-#######
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=72
-#SBATCH --partition=normal
-#SBATCH --mem=80G
-#SBATCH --time=48:00:00 
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=a_toen03@uni-muenster.de
-#SBATCH --job-name=ATACseq_Fq2Bam
-#######
- 
 ######################################################################################################################################
 
 ## Exit function if BAM file looks corrupted or is missing:
@@ -285,5 +281,3 @@ conda deactivate py27_env
 
 ## Summary report:
 multiqc -o multiqc_all ./ 
- 
-
