5259a97f007ea8b18be1bb723bb21e56e465f468
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-02-11 21:39:36 +0100

    Update hisat2.sh

--- RNA-seq/hisat2.sh
+++ RNA-seq/hisat2.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
 
-## Simple RNA-seq pipeline, accepting paired-end fastq files, producing sorted HISAT2 alignment.
-## Written by Alexander Toenges (a.toenges@uni-muenster.de) 2017
-## USAGE: RNAseq_hisat2.sh <Basename>
+## Align paired-end RNA-seq data with HISAT2:
 
 ################################################################################################################################################
-################################################################################################################################################
-
-echo '[INFO]' $1 'Started on date:' && date && echo ''
 
 BASENAME=$1
 
 ## HISAT2 index, splice-site file and genome GTF:
-HISAT2_IDX="/scratch/tmp/a_toen03/Genomes/hg38/hisat2_IDX/hg38_noALT_withDecoy"
-SPLICE_FILE="/scratch/tmp/a_toen03/Genomes/hg38/hisat2_IDX/gencode.v27.annotation_SpliceSites.txt"
+HISAT2_IDX="/scratch/tmp/a_toen03/Genomes/mm10/hisat2_index/mm10"
+SPLICE_FILE="/scratch/tmp/a_toen03/Genomes/mm10/hisat2index/mm10_spliceSites.txt"
+GENEMODEL="/scratch/tmp/a_toen03/Genomes/mm10/Gencode_M20/GRCm38_mm10_Ensembl_GeneModel.bed"
 
-## Check if fastq files are present
-if [[ ! -e ${BASENAME}_1.fastq.gz ]] || [[ ! -e ${BASENAME}_1.fastq.gz ]]; then echo '[ERROR]: Input files not present -- exiting'; fi
-
-################################################################################################################################################
-################################################################################################################################################
-
-##: Trim adapters with skewer
-# --- Illumina's paired-end adapters ::: -x fwd -y rev
-# --- remove degenerated (NNNNN) reads ::: -n
-# --- trim 3' until hitting a base with qual >= 20 ::: -q 20
-# --- remove reads with mean quality below 20 ::: -Q 20
-# --- multithreaded ::: -t INT
-
-## Prepare output directory:
-echo '[MAIN]: Adapter/Quality trim for sample' $1
-skewer -m pe -n --quiet -q 20 -Q 20 -t 8 -o ${1} ${1}_1.fastq.gz ${1}_2.fastq.gz
-
-################################################################################################################################################
-################################################################################################################################################
-
-## Check for output directory:
-if [[ ! -d BAM ]]; then mkdir BAM; fi
-
-## Align with HISAT2, without sorting, as mostly BAMs are only used for featureCounts quant 
-## and that requires name-sorting anyway:
-echo '[MAIN]: HISAT2 for sample' $1
-hisat2 -p 32 -X 1000 --known-splicesite-infile $SPLICE_FILE --summary-file ${1}_hisat2_report.log -x $HISAT2_IDX -1 ${1}-trimmed-pair1.fastq -2 ${1}-trimmed-pair2.fastq | \
-  samblaster | \
-  sambamba view -S -f bam -l 5 -t 4 -o ./BAM/${1}_unsorted.bam /dev/stdin && rm ${1}-trimmed*.fastq
+function HisatALN {
   
-################################################################################################################################################
-################################################################################################################################################
+  BASENAME=$1 
+  
+  ## Check if fastq files are present
+  if [[ ! -e ${BASENAME}_1.fastq.gz ]] || [[ ! -e ${BASENAME}_2.fastq.gz ]]; then
+    (>&2 paste -d " " <(echo '[ERROR]' 'Missing input files for' $BASENAME ' -- exit') <(date))
+    exit 1
+    fi
+  
+  (>&2 paste -d " " <(echo '[INFO]' 'HisatALN for' $1 'started on') <(date))
 
-echo '[INFO]' $1 'Ended on date:' && date 
+  skewer -m pe -n --quiet -q 20 -Q 20 -t 8 -o ${BASENAME} ${BASENAME}_1.fastq.gz ${BASENAME}_2.fastq.gz
+
+  hisat2 -p 12 -X 1000 \
+    --known-splicesite-infile $3 \
+    --summary-file ${1}_hisat2_report.log \
+    -x $2 \
+    -1 ${BASENAME}-trimmed-pair1.fastq \
+    -2 ${BASENAME}-trimmed-pair2.fastq | \
+  samblaster | \
+  sambamba view -S -f bam -l 5 -t 4 -o /dev/stdout /dev/stdin | \
+  sambamba sort -m 4G --tmpdir=./ -l 6 -t 12 -o /dev/stdout /dev/stdin | \
+  tee ${BASENAME}_sorted.bam | \
+  tee >(samtools flagstat - > ${BASENAME}_sorted.flagstat) | \
+  samtools index - > ${BASENAME}_sorted.bam.bai
+  
+  ## QC:
+  read_distribution.py -i ${BASENAME}_sorted.bam -r $4 > ${BASENAME}_sorted_readDistro.txt
+  read_duplication.py -i ${BASENAME}_sorted.bam -o ${BASENAME} -q 20
+    
+}; export -f HisatALN 
+
+ls *.fastq.gz | parallel -j 72 "fastqc {}"
+
+ls *_1.fastq.gz | awk -F "_1" '{print $1}' | sort -u | \
+  parallel -j 4 "HisatALN {} $HISAT2_IDX $SPLICE_FILE $GENEMODEL 2>> {}.log"
