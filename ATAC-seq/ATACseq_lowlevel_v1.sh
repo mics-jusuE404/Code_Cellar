53c4bb9daa567967dbbf94ed4f19d0f347a611b8
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-02-12 15:00:33 +0100

    print sizeFactors.R script to disk
    added an echo that saves the sizeFactors.R script to disk. that avoids that the script must be manually copied to the $pwd of this script which i always forget
    
    also added path to macs2 instead of activating py27env

--- ATAC-seq/ATACseq_lowlevel_v1.sh
+++ ATAC-seq/ATACseq_lowlevel_v1.sh
@@ -20,6 +20,43 @@ MODE="PE"
 ## Don't forget to set appropriate genome version flag!
 
 ######################################################################################################################################
+## print script for sizefactors:
+
+echo '
+#!/home/a/a_toen03/anaconda3_things/anaconda3/envs/R_env/bin/Rscript
+
+## Script to calculate DESeq2 size factors for ATAC-seq data based on a count matrix
+## over the entire genome with 500bp windows, taking the top 100k windows based on rowMeans.
+## File comes from stdin via the main bash script ATACseq_lowlevel(...).sh
+
+suppressMessages(library(DESeq2))
+suppressMessages(library(data.table))
+ 
+## read data:
+counts <- fread('cat /dev/stdin', skip = 1, header = T, data.table = F)
+
+## get rowMeans
+counts <- data.frame(counts, rowMean = rowMeans(counts[,7:ncol(counts)]))
+
+## sort by rowMeans
+counts <- counts[with(counts, order(-rowMean)),]
+
+## top 100k regions based on rowMeans:
+counts_subset <- head(counts[,7:(ncol(counts)-1)], n=100000)
+
+## size factors:
+SizeFactors <- estimateSizeFactorsForMatrix(counts = counts_subset)
+
+## write:
+write.table(data.frame(SizeFactors), sep="\t", col.names = F, row.names = T,
+quote = F, file = "./sizeFactors.txt")
+' > sizeFactors.R
+
+
+
+
+
+######################################################################################################################################
 
 ## Exit function if BAM file looks corrupted or is missing:
 function ExitBam {
@@ -272,10 +309,10 @@ if [[ $MODE == "SE" ]]; then
 ####################################################################################################################################
 
 ## Peaks for each sample:
-conda activate py27_env
+MACS="/anaconda3_things/anaconda3/envs/py27_env/bin/macs2"
 ls *dedup.bam | \
- parallel "macs2 callpeak -t {} -n {.} -g mm --extsize 150 --shift -75 --nomodel --keep-dup=all -f BAM"
-conda deactivate py27_env
+ parallel "$MACS callpeak -t {} -n {.} -g mm --extsize 150 --shift -75 --nomodel --keep-dup=all -f BAM"
+
 
 ####################################################################################################################################
 
