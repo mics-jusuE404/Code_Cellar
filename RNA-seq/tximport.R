12f76991f13e0a6ec392ab1ff65e820e3a34ddeb
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-02-27 13:28:14 +0100

    Update tximport.R

--- RNA-seq/tximport.R
+++ RNA-seq/tximport.R
@@ -12,33 +12,26 @@ require(data.table)
 ## Gencode v28 Tx2Gene
 TX2Gene <- fread("/scratch/tmp/a_toen03/Genomes/hg38/Gencode_v28/gencode.v28.TX2Gene.txt", header = T, data.table = F)
 
-## Table that indicates which SRRs belong to one patient
-Patient   <- fread("path/to/dir/table1.tsv", data.table = F, header = T)
-Cellline  <- fread("path/to/dir/table2.tsv", data.table = F, header = F)
-
-
-################################################################################################################################################
-
-## Tximportfiles:
-TxImport_AT <- function(QuantDir){
+## Assuming the *_salmon folders are in a folder termed <Salmon>:
+TxImport_Custom <- function(QuantDir){
   
-  ## Get the paths for all the quantification files and then rename them to only get the SRR-ID,
-  ## assuming they are all in a folder (...)/fastq and have name SRR(...)_salmonK${IDXLENGTH}:
-  tmp.files <- list.files(path = list.dirs(QuantDir, recursive=FALSE), pattern = "quant.sf", full.names = T)
-  names(tmp.files) <- sapply(strsplit(tmp.files, split = "/fastq/|_salmon"), function(x) x[2])
-  names(tmp.files) <- gsub("/", "", names(tmp.files))
-  return(tmp.files)
+  ## List folders and get names:
+  message("Checking for quantification files")
+  tmp.files <- list.files(path = list.dirs(path = QuantDir, recursive=FALSE), pattern = "quant.sf", full.names = T)
+  names(tmp.files) <- sapply(strsplit(tmp.files, split = "/|_salmon"), function(x) x[3])
+  
+  ## Do tximport:
+  if ( exists("TX2Gene") == FALSE) stop("TX2Gene does not exist")
+  message("Importing quantifications")
+  return(
+    tximport(files = tmp.files,
+             type = "salmon",
+             txIn = T, txOut = F, 
+             tx2gene = TX2Gene)
+  )
+  
 }
 
-## Patients:
-Salmons_Patients <- TxImport_AT(QuantDir = "./Patients/fastq/")
-Salmons_Lines    <- TxImport_AT(QuantDir = "./Cell_Lines/fastq/")
-
-
 ################################################################################################################################################
 
-## Import everthing:
-Txi_all <- tximport(files = unlist(sapply(ls(pattern = "Salmons_"), get)),
-                    type = "salmon",
-                    txIn = T, txOut = F, 
-                    tx2gene = TX2Gene)
+ImmGenRNA_tximport <- TxImport_Custom(QuantDir = "RNA-seq/Salmons")
