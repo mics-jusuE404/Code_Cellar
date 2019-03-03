f344a618d55741e5955104e01e02acab98519c75
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-03-03 14:45:55 +0100

    Update ATACseq_lowlevel_v4.sh

--- ATAC-seq/ATACseq_lowlevel_v4.sh
+++ ATAC-seq/ATACseq_lowlevel_v4.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=72
 #SBATCH --partition=normal
-#SBATCH --mem=80G
+#SBATCH --mem=100G
 #SBATCH --time=48:00:00 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=a_toen03@uni-muenster.de
