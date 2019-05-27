3f3db675a958175e6e3cbb21e53b39beb2103d9d
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-05-27 11:47:49 +0200

    Update plotMA_custom_v1.R

--- Differential_Analysis/plotMA_custom_v1.R
+++ Differential_Analysis/plotMA_custom_v1.R
@@ -10,62 +10,62 @@ plotMA_custom <- function(COUNTS,           ## 2-column data.frame or matrix wit
                           MAIN = "",        ## plot title
                           MODE = "smooth",  ## smooth for smoothScatter plot or heatscatter for LSD::heatscatter
                           YLIM="",          ## user-defined ylims, if empty will be chosen based on quantiles
-                          REPLACE.ZERO = 1) ## uses the specified number (default 1) to replace zeros to avoid log2(0) {
+                          REPLACE.ZERO = 1){ ## uses the specified number (default 1) to replace zeros to avoid log2(0) 
   
-  par(bty="n")
-  ## Check if LSD is installed:
-  if (MODE == "heatscatter" && !("LSD" %in% rownames(installed.packages()))){
-    message("LSD package is not installed, using smoothScatter instead")
-    MODE <- "smooth"
-  }
+par(bty="n")
+## Check if LSD is installed:
+if (MODE == "heatscatter" && !("LSD" %in% rownames(installed.packages()))){
+  message("LSD package is not installed, using smoothScatter instead")
+  MODE <- "smooth"
+}
+
+## F if provided counts are on the regular (=non-log) scale
+if (LOGGED == F){
+  R=COUNTS[,1]
+  G=COUNTS[,2]  
   
-  ## F if provided counts are on the regular (=non-log) scale
-  if (LOGGED == F){
-    R=COUNTS[,1]
-    G=COUNTS[,2]  
-    
-    R[which(R == 0)] <- REPLACE.ZERO
-    G[which(G == 0)] <- REPLACE.ZERO
-  }
-  
-  ## T if counts are already on log2 scale
-  if (LOGGED == T){
-    R=2^COUNTS[,1]
-    G=2^COUNTS[,2]  
-  }
-  
-  ## get mean of counts and log2FC
-  M <- log2(R/G)
-  A <- 0.5*log2(R*G)
-  
-  ## Decide y-axis limits based on rounded quantiles
-  if (YLIM[1] == ""){
-    YLIM <- c(floor(quantile(M, 0.0001, na.rm=T)), ceiling(quantile(M, 0.9999, na.rm=T)))
-  }
-  
-  ## using smoothScatter
-  if (MODE == "smooth"){
-    smoothScatter(A, M, main = MAIN, bty="n",
-                  xlab="mean of normalized counts",
-                  ylab="log2FC", ylim=YLIM)
-    abline(h=0)  
-  }
-  
-  ## using heatscatter (more fancy but pdfs will be much larger)
-  if (MODE == "heatscatter"){
-    library(LSD)
-    heatscatter(A, M, main = MAIN, bty="n", pch=20,
+  R[which(R == 0)] <- REPLACE.ZERO
+  G[which(G == 0)] <- REPLACE.ZERO
+}
+
+## T if counts are already on log2 scale
+if (LOGGED == T){
+  R=2^COUNTS[,1]
+  G=2^COUNTS[,2]  
+}
+
+## get mean of counts and log2FC
+M <- log2(R/G)
+A <- 0.5*log2(R*G)
+
+## Decide y-axis limits based on rounded quantiles
+if (YLIM[1] == ""){
+  YLIM <- c(floor(quantile(M, 0.0001, na.rm=T)), ceiling(quantile(M, 0.9999, na.rm=T)))
+}
+
+## using smoothScatter
+if (MODE == "smooth"){
+  smoothScatter(A, M, main = MAIN, bty="n",
                 xlab="mean of normalized counts",
                 ylab="log2FC", ylim=YLIM)
-    abline(h=0)  
-  }
-  
-  ## plot points beyond y-axis limits as triangles to the y-axis limits
-  if (MODE == "smooth") COL = "darkblue"
-  if (MODE == "heatscatter") COL = "firebrick"
-  points(x = A[which(M < YLIM[1])], y = rep(YLIM[1], length(which(M < YLIM[1]))), pch=17, cex = 0.7, col=COL)
-  points(x = A[which(M > YLIM[2])], y = rep(YLIM[2], length(which(M > YLIM[2]))), pch=17, cex = 0.7, col=COL)
-  
+  abline(h=0)  
+}
+
+## using heatscatter (more fancy but pdfs will be much larger)
+if (MODE == "heatscatter"){
+  library(LSD)
+  heatscatter(A, M, main = MAIN, bty="n", pch=20,
+              xlab="mean of normalized counts",
+              ylab="log2FC", ylim=YLIM)
+  abline(h=0)  
+}
+
+## plot points beyond y-axis limits as triangles to the y-axis limits
+if (MODE == "smooth") COL = "darkblue"
+if (MODE == "heatscatter") COL = "firebrick"
+points(x = A[which(M < YLIM[1])], y = rep(YLIM[1], length(which(M < YLIM[1]))), pch=17, cex = 0.7, col=COL)
+points(x = A[which(M > YLIM[2])], y = rep(YLIM[2], length(which(M > YLIM[2]))), pch=17, cex = 0.7, col=COL)
+
 }
 
 ## Example usage:
