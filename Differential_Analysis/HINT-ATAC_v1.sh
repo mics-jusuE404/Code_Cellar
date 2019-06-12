b8abbaf76f894609d9089277adbc3f86c7238c66
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-06-12 15:22:17 +0200

    Create HINT-ATAC_v1.sh

---
+++ Differential_Analysis/HINT-ATAC_v1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## Wrapper script to perform differential footprinting analysis on ATAC-seq data with HINT-ATAC
+(https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1642-2)
