9adeac39b712679e793d67ad430da9a5e433362e
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-07-30 16:53:37 +0200

    Update FindMostAbundantTxPerGene_V1.R

--- RNA-seq/FindMostAbundantTxPerGene_V1.R
+++ RNA-seq/FindMostAbundantTxPerGene_V1.R
@@ -1,67 +1,43 @@
-## Naive approach to find most abundant transcript per gene for one or more samples/replicates,
-## based on TPMs from salmon transcript quantifications. 
-## Returns a data.frame with the Tx name for each replicate group or sample.
+## Take salmon quantifications for specified samples, average TPMs and then select the most abudant Tx per gene:
 
 FindMostAbundantTxPerGene <- function(SalmonDir,             ## directory where salmon output folders are, expected to follow *_rep*_salmon
-                                      AggregateReps = TRUE,  ## get most abundant tx per gene based on replicate average TPM
-                                      Tx2Gene = NULL){       ## if T use default mm10 gencode v20
+                                      SampleName,            ## the SampleName of the samples to be considered
+                                      Tx2Gene){              ## file with $1 the ensembl tx name and $2 the ensembl gene ID without header
   
   #########################################################################################################################################
   
   library(data.table)
   
-  #########################################################################################################################################
-  
-  ## Default mm10 Gencode v20, matching ensembl Tx and gene IDs:
-  if (is.null(Tx2Gene)) {
-    ensmus_tx2gene <- fread("/Volumes/Rumpelkammer/Genomes/mm10/Gencode_M20/ENSMUST_2_ENSMUSG.txt", 
-                            data.table = F, 
-                            header = F)
-  }
+  colnames(Tx2Gene) <- c("V1", "V2")
+  Tx2Gene$V1 <- sapply(strsplit(as.character(Tx2Gene$V1), split="\\."), function(x)x[1])
+  Tx2Gene$V2 <- sapply(strsplit(as.character(Tx2Gene$V2), split="\\."), function(x)x[1])
   
   #########################################################################################################################################
   
-  ## Scan salmon dirs for replicates:
-  if (AggregateReps == TRUE){
-    
-    tmp.names <- list.files(SalmonDir, pattern = "_salmon$")  
-    unique.basenames <- as.character(unique(sapply(strsplit(tmp.names, split="_rep"), function(x)x[1])))
-    
-    #######################################################################################################################################
-    
-    tmp.mostAbundant <- sapply(unique.basenames, function(x){
-      
-      ## list all files that have the same basename:
-      tmp.reps <- grep(paste0("^", x, "_rep"), list.files(SalmonDir, pattern = "_salmon$"), value = TRUE)
-      
-      ## get the tx-gene-tpm table for each replicate of a group:
-      tmp.byReplicates <- 
-      lapply(tmp.reps, function(m){
-        tmp.read  <- fread(paste0(SalmonDir, "/", m, "/quant.sf"), data.table = F, header = T, select = c(1,4))
-        tmp.merge <- merge(ensmus_tx2gene, tmp.read, by.x = "V1", by.y = "Name")
-      })
-      
-      tmp.df <- do.call(cbind, tmp.byReplicates)
-      tmp.df <- data.frame(tmp.df[,c(1,2, grep('TPM', colnames(tmp.df)))])
-      
-      if ( length(grep('TPM', colnames(tmp.df))) > 1){
-        tmp.df <- data.frame(tmp.df[c(1,2)], TPM = rowMeans(tmp.df[,seq(3, ncol(tmp.df))]))  
-      }
-      
-      ## keep most abundant (by TPM) transcript per gene:
-      tmp.df <- tmp.df[order(-tmp.df$TPM) , ] #sort by id and reverse of abs(value)
-      return( tmp.df[ !duplicated(tmp.df[,2]), 1] ) 
-      
-    })
-    
-    
-    #######################################################################################################################################
+  ## Scan salmon dirs for the intended data:
+  tmp.names <- grep(paste0("^", SampleName), list.files(SalmonDir, pattern = "_salmon$"), value = T)  
+  
+  if (length(tmp.names) > 1){
+    tmp.df<-do.call(cbind,
+            lapply(tmp.names, function(m){
+      return(fread(paste0(SalmonDir, "/", m, "/quant.sf"), data.table = F, header = T, select = c(1,4)))
+    }))
+    tmp.df <- tmp.df[,c(1,grep("TPM", colnames(tmp.df)))]
+    tmp.df <- data.frame( ENSMUST = sapply(strsplit(as.character(tmp.df$Name), split="\\."), function(x)x[1]),
+                          AverageTPM = rowMeans(tmp.df[,grep("TPM", colnames(tmp.df))]))
     
   }
   
-  return( data.frame( tmp.mostAbundant ))
+  ## add genes:
+  tmp.df <- merge(x = tmp.df, y = Tx2Gene, by.x="ENSMUST", by.y = "V1", sort = FALSE)
   
-}
+  ## keep only most abundant transcript per gene
+  tmp.df <- tmp.df[order(-tmp.df$AverageTPM) , ]
   
-a<- FindMostAbundantTxPerGene(SalmonDir = "~/IMTB/Project_Leukemia/RNAseq_July2019/salmons/")
+  tmp.df <- tmp.df[ !duplicated(tmp.df$V2), ]
   
+  tmp.df <- tmp.df[,c(1,3,2)]
+  
+  colnames(tmp.df) <- c("ENSMUST", "ENSMUSG", "TPM")
+  return(tmp.df) 
+  }
