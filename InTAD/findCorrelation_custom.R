468b44467bb9bb1e5003d1efdf0336deac03c0bd
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-08-10 17:12:09 +0200

    changed do.call(rbind) to data.table::rbindlist(), 2 as fast

--- InTAD/findCorrelation_custom.R
+++ InTAD/findCorrelation_custom.R
@@ -1,8 +1,5 @@
-## This is the InTAD::findCorrelations.R script, modified to enable chunk-wise processing
-## of signal/gene pairs to reduce memory consumption.
 
-## This function is unchanged but renamed it to avoid conflicts
-findGeneCorrelation_custom <- function(x, signalVals, countVals, corMethod) {
+findGeneCorrelation <- function(x, signalVals, countVals, corMethod) {
     genes <- x$geneid
     pId <- as.character(unique(x$peakid))
     #print(pId)
@@ -31,18 +28,29 @@ findGeneCorrelation_custom <- function(x, signalVals, countVals, corMethod) {
     return(corDF)
 }
 
-## Custom function to perform correlation analysis splitted over chunk.sizes elements per go
-## to reduce memory consumption
-findCorrelation_custom <- function(object,               ## InTADSig object
-                                   chunk.size = 1000,    ## number of elements per chunk   
-                                   method = "pearson",   ## correlation method
-                                   Studyname,            ## studyname for output to disk ./InTAD_raw/InTAD_raw_Studyname_permutcycle_chunk(n).tsv
-                                   current.cycle = NULL, ## from the lapply function that makes the permutations the current cycle
-                                                         ## only needed to print correct file name
-                                   total.cycle = NULL,
-                                   Cores = 16
-                                   )
-  {
+
+#' Function to perfrom correlation analysis
+#'
+#' This function combines genes and signals in inside of TADs
+#' @param object InTADSig object with signals and genes combined in TADS
+#' @param method Correlation method: "pearson" (default), "kendall", "spearman"
+#' @param adj.pval Perform p-value adjsutment and include q-values in result
+#' @param plot.proportions Plot proportions of signals and genes in correlation
+#' @return A table with correlation values for signal-gene pairs including
+#' correlation p-value, euclidian distance and rank.
+#' @importFrom stats na.omit cor.test dist
+#' @import qvalue
+#' @export
+#' @examples
+#' ## perform analysis on test data
+#' inTadSig <- newSigInTAD(enhSel, enhSelGR, rpkmCountsSel, txsSel)
+#' inTadSig <- filterGeneExpr(inTadSig, geneType = "protein_coding")
+#' inTadSig <- combineInTAD(inTadSig, tadGR)
+#' corData <- findCorrelation(inTadSig, method="pearson")
+#'
+
+findCorrelation <- function(object, method = "pearson", adj.pval = FALSE,
+                            plot.proportions = FALSE ) {
 
     if (!is(object, "InTADSig"))
         stop("Object must be an InTADSig!")
@@ -50,32 +58,33 @@ findCorrelation_custom <- function(object,               ## InTADSig object
     if (length(object@signalConnections)  == 0)
         stop("No signals and genes are combined in TAD!
             Use combineInTAD() function.")
-    
-    
-    
+
     sigCons <- object@signalConnections
     txs <- rowRanges(object@sigMAE[["exprs"]])
-    #this null filtering was already performed in previous step
+
+    # this null filtering was already performed in previous step
     allIdGnX <- sigCons[!sapply(sigCons, is.null)]
     allIdGnY <- allIdGnX[sapply(allIdGnX, function(x) dim(x)[1]) > 0]
-    ## </turn off (ATpoint)>
-    
-    ## Modification (ATpoint), always do parallel processing and chunk-wise:
-    
-    assay.signal <- assay(object@sigMAE[["signals"]])
-    assay.exprs  <- assay(object@sigMAE[["exprs"]])
-    
-    ## This function does the postprocessing of the raw correlation output and is basically
-    ## same as in original script but removed plotting and qvalue parts.
-    ## q-value calculation will be done later manually
-    AfterCor <- function(allRes=allRes){
-      
-      allRes <- lapply(allRes, function(x) na.omit(x))
-      
-      allX <- list()
-      for(i in seq_len(length(allRes))) {
+
+    if (object@ncore > 1 && requireNamespace("parallel")) {
+        message("Running in parallel...")
+        allRes <- parallel::mclapply(allIdGnY, findGeneCorrelation,
+                assay(object@sigMAE[["signals"]]),
+                assay(object@sigMAE[["exprs"]]),
+                method)
+    } else {
+        allRes <- lapply(allIdGnY, findGeneCorrelation,
+                    assay(object@sigMAE[["signals"]]),
+                    assay(object@sigMAE[["exprs"]]),
+                    method)
+    }
+
+    allRes <- lapply(allRes, function(x) na.omit(x))
+
+    allX <- list()
+    for(i in seq_len(length(allRes))) {
         gnname <- values(txs)$gene_name[match(allRes[[i]]$gene,
-                                              values(txs)$gene_id)]
+                                            values(txs)$gene_id)]
         rnk <- allRes[[i]]$gene[order(allRes[[i]]$corr, decreasing=TRUE)]
         corRank <- match(allRes[[i]]$gene, rnk)
         #cluster <- cltab$cluster[match(names(allRes)[i], cltab$id)]
@@ -83,67 +92,58 @@ findCorrelation_custom <- function(object,               ## InTADSig object
         tad <- unique(allIdGnX[[peakName]]$tad)
         #tad <- inTADpairs$tad[match(names(allRes)[i], inTADpairs$peakid)]
         allX[[i]] <- data.frame(peakid=rep(peakName, length(gnname)),
-                                tad=rep(tad,length(gnname)),
-                                gene=allRes[[i]]$gene, name=gnname,
-                                cor=allRes[[i]]$corr,
-                                pvalue=allRes[[i]]$pvalue,
-                                eucDist = allRes[[i]]$eudis,
-                                corRank=corRank,
-                                stringsAsFactors =FALSE)
-      }
-      
-      return( do.call(rbind, lapply(allX, function(x) x)) )
+                            tad=rep(tad,length(gnname)),
+                            gene=allRes[[i]]$gene, name=gnname,
+                            cor=allRes[[i]]$corr,
+                            pvalue=allRes[[i]]$pvalue,
+                            eucDist = allRes[[i]]$eudis,
+                            corRank=corRank,
+                            stringsAsFactors =FALSE)
     }
-    
-    ## Initialize by deviding into how many 1000er-chunks the process is split,
-    ## if fewer than 1000 present set to 1:
-    tmp.lower <- 1
-    tmp.upper <- chunk.size
-    total.chunks <- ceiling(length(allIdGnY) / tmp.upper)
-    trash.can <- lapply(X = seq(1, total.chunks),
-           FUN = function(CHUNK){
-             
-             message(
-               paste("[Info]: Processing chunk", 
-                     CHUNK, "of", total.chunks, 
-                     "for cycle", current.cycle, "of", total.cycle)
-               )
-             
-             if (tmp.upper > length(allIdGnY)) tmp.upper <- length(allIdGnY)
-        
-             allIdGnY.use <- allIdGnY[names(allIdGnY)[c(seq(tmp.lower, tmp.upper))]]
-      
-             allRes <- parallel::mclapply(allIdGnY.use,
-                                          findGeneCorrelation_custom,
-                                 assay.signal,
-                                 assay.exprs,
-                                 method,
-                                 mc.cores=Cores)
-              
-             ## Initialize next round:
-             tmp.lower <- tmp.upper + 1
-             tmp.upper <- tmp.upper*2
-      
-             ## post processing
-             tmp.result <- AfterCor(allRes = allRes)
-             
-             if (CHUNK == 1) true.false <- TRUE
-             if (CHUNK > 1)  true.false <- FALSE
-             
-             if (is.null(current.cycle)) current.cycle <- 1
-             
-             ## write results to avoid keeping it in memory
-             if (!dir.exists("./InTAD_raw")) dir.create("./InTAD_raw")
-             write.table(x = tmp.result, 
-                         append = !true.false, 
-                         file = paste0("./InTAD_raw/", "InTAD_raw_", Studyname, "_permut", current.cycle, "_chunk_", CHUNK, ".tsv"),
-                         quote = FALSE,
-                         sep = "\t",
-                         row.names = FALSE,
-                         col.names = true.false)
-             rm(tmp.result, allRes)
-    
-      } ## end for-loop
-    )
-    message("[Info]: Done processing all chunks")
+
+    allCortab <- do.call(rbind, lapply(allX, function(x) x))
+
+    if (adj.pval) {
+        qvals <- tryCatch({
+            qvalue(allCortab$pvalue)
+        }, error = function(e) {
+            qvalue(allCortab$pvalue, pi0=1)
+        })
+        allCortab <- cbind(allCortab, qvals$qvalues)
+        colnames(allCortab)[9] <- "qvalue"
+    }
+
+    if (plot.proportions) {
+        corDataF <- allCortab[allCortab$pvalue < 0.05,]
+        if (nrow(corDataF) == 0) {
+            warning("No conections detected")
+            return(allCortab)
+        }
+        # plot 1
+        assignedGeneProportion <- summary(as.factor(corDataF$gene),
+                                            maxsum = nrow(corDataF))
+        agpSorted <- sort(assignedGeneProportion, decreasing = TRUE)
+
+        h <- hist( agpSorted, plot =FALSE )
+        h$density = h$counts/sum(h$counts)*100
+        plot(h, freq=FALSE, xlab="Number of signals",
+            ylab = "Assigned genes (%)",
+            col="blue",cex.main = 0.7,cex.axis=0.8,cex.lab=0.7,
+            main = "Genes regulated by signals (p-val 0.05)")
+
+        # plot 2
+        assignedEnhProportion <- summary(as.factor(corDataF$peakid),
+                                            maxsum = nrow(corDataF))
+        aepSorted <- sort(assignedEnhProportion, decreasing = TRUE)
+
+        h <- hist( aepSorted, plot =FALSE )
+        h$density = h$counts/sum(h$counts)*100
+        plot(h, freq=FALSE, xlab="Number of genes",
+            ylab = "Assigned signals (%)",
+            col="blue", cex.main = 0.7,cex.axis=0.8,cex.lab=0.7,
+            main = "Signals regulating genes (p-val 0.05)")
+
+    }
+
+    return(allCortab)
 }
