f6a9073f51f45b111af74f00cdf8c1e1be54fdcf
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2019-08-16 11:19:07 +0200

    optional reordering of rows according to reference
    Can now order the rows of the heatmap by either the tools default, or by decreasing enrichment score of a reference file or completely controlled by a user-supplied order.

--- Heatmap/EnrichedHeatmap_v1.R
+++ Heatmap/EnrichedHeatmap_v1.R
@@ -3,8 +3,10 @@
 ## FIRST DRAFT, not finished yet!
 
 plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths (on disk)
-                         bigwig.names = NULL,         ## a list of length(bigwigs) with the names to be used \
-                         ## as plot title 
+                         bigwig.names = NULL,         ## a list of length(bigwigs) with the names to be used as plot title 
+                         reference.bigwig = NULL,     ## the bigwig.names element to serve as the reference towards row order
+                         return.roworder = FALSE,     ## whether to return the row order from the reference bigwig to use for other plots
+                         given.roworder = NULL,       ## a vector or list of length(targets.gr) to use for row ordering of the plot
                          targets.gr,                  ## GRanges with target regions 
                          target.name,                 ## a name (e.g. cluster2) that will be used as part of file name for the pdf
                          window.size,                 ## window size [bp] to extend targers.gr center positions
@@ -15,7 +17,6 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
                          log2.prior = 1,              ## prior count in case of do.log2
                          col.low = "darkblue",        ## color to start gradient for low counts
                          col.high = "darkgoldenrod1", ## color to end gradient for high counts
-                         main.title = NULL,           ## NULL to use bigwig.names as main plot title
                          font.fam = "sans",           ## default font
                          font.size.title = 15,        ## the font size for the main title above the profile plot
                          legend.title                 ## the name for the legend, e.g. "ATAC-seq [CPM]" or "H3K27ac [CPM]"
@@ -30,7 +31,7 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
   }
   
   ## Package checks:
-  packageS <- c("rtracklayer", "GenomicRanges", "EnrichedHeatmap")
+  packageS <- c("rtracklayer", "GenomicRanges", "EnrichedHeatmap", "circlize")
   if (length(grep("FALSE", (packageS %in% rownames(installed.packages())))) > 0){
     message("[Error]: Package(s) { ", 
             paste(packageS[which( packageS %in% rownames(installed.packages()) == "FALSE")],collapse = " " ), 
@@ -41,6 +42,8 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
   require(rtracklayer)
   require(GenomicRanges)
   require(EnrichedHeatmap)
+  require(circlize)
+  
   set.seed(2019)
   
   #######################################################################################################################################
@@ -52,6 +55,25 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
       message("[Error]: Length of BigWig and bigwig.names is not the same!")
       stop_quietly()
     }
+  }
+  
+  if(!is.null(reference.bigwig) & !is.null(given.roworder)){
+    message("[Error]: Both a reference.bigwig and a given.order is provided. Decide for one!")
+    stop_quietly()
+  }
+  
+  ## check that the name of reference bigwig is in bigwig.names:
+  if(!is.null(reference.bigwig)){
+    if (!reference.bigwig %in% bigwig.names){
+      message("[Error]: reference.bigwig is not part of bigwig.names!")
+      stop_quietly()
+    }
+  }
+  
+  ## if order is provided make sure is same length as target regions:
+  if (!is.null(given.roworder) & ( length(targets.gr) != length(unlist(given.roworder)))){
+    message("[Error]: Length of given.order is not the same as of targets.gr!")
+    stop_quietly()
   }
   
   if (!scale.all) {
@@ -78,17 +100,13 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
     message("[Error]: Those bigwigs do not exist: ", paste(noBig, collapse = " "))
     stop_quietly()
   }
-  
-  if (!is.null(main.titles) && (length(main.titles) != length(bigwigs))){
-    message("[Error]: main.titles can either be NULL or a vector of same length as bigwigs!")
-    stop_quietly()
-  }
-  
+
   if (is.null(legend.title)){
     message("[Error]: Enter a title for the legend!")
     stop_quietly()
   }
   
+  ## error if indicated color do not exist in R color collection:
   if (length(
     grep(paste0(paste0("^", c(col.low, col.high), "$"), collapse = "|"), colors())
   ) < 2) {
@@ -112,7 +130,7 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
     nM <- normalizeToMatrix(signal = tmp.bigwig, 
                             target = tmp.target0, 
                             background = 0, 
-                            keep = c(0, 0.99), ## to exclude any outliers from skewing the plot 
+                            keep = c(0, 0.99), ## 99th eprcentile to orevent any outliers from skewing the plot 
                             target_ratio = 0,
                             mean_mode = "w0", 
                             value_column = "score", 
@@ -132,7 +150,7 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
       tmp.min <- min(tmp.cm)
       tmp.max <- max(tmp.cm)
       data.frame(Min=tmp.min, Max=tmp.max)
-    }
+      }
     )
     
     ## Use 0 and 10% added to tmp.upper as limits:
@@ -153,17 +171,41 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
     
   }
   
+  #### determine row order of the heatmap, depending on the settings:
+  
+  ##-case: no reference file and no given.order, so every plot is ordered by its decreasing enrichment score
+  if (scale.all & is.null(given.roworder)){
+    tmp.order <<- NULL
+  }
+  
+  ##-case: a sort order is given
+  if (scale.all & !is.null(given.roworder)){
+    tmp.order <<- unlist(given.roworder)
+  }
+  
+  ##-case: sort by decreasing enrichment score of the reference.bigwig:
+  if (scale.all & !is.null(reference.bigwig)){
+    
+    which.isRef  <- grep(paste0("^", reference.bigwig, "$"), bigwig.names)
+    tmp.enrScore <- enriched_score(tmp.nM[[which.isRef]])
+    tmp.order    <<- sort(tmp.enrScore, index.return = TRUE, decreasing = TRUE)$ix
+    
+  }
+  
   ## Now plot everything:
   trashy.can <- mclapply(1:length(tmp.nM), mc.cores = detectCores()-1,function(p){
     
-    ## default is to use bigwig.names as plot title
-    if (is.null(main.titles)) tmp.title  = bigwig.names[p]
-    ## or a user-provided list
-    if (!is.null(main.titles)) tmp.title = main.titles[p]
+    ## if no names are given use the basename of the bigwig as plot/legend name:
+    if (!is.null(bigwig.names)) tmp.name <- bigwig.names[p]
+    if (is.null(bigwig.names)) tmp.name <- tools::file_path_sans_ext(basename(bigwigs[p]))
+    
+    if (is.null(tmp.order))  tmp.order <- order(enriched_score(tmp.nM[[p]]), decreasing = TRUE)
     
     ## heatmap function:
     enrHtmp <- EnrichedHeatmap(
       mat = tmp.nM[[p]], 
+      
+      row_order = tmp.order, ## the row order of the heatmap (tmp.order see above)
       
       pos_line = FALSE, ## no dashed lines around the start
       
@@ -171,7 +213,7 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
       
       col = col_fun,    ## color gradients
       
-      column_title = tmp.title, ## column title 
+      column_title = tmp.name, ## column title 
       column_title_gp = gpar(fontsize = font.size.title, fontfamily = font.fam),
       
       ## use raster to make high quality but powerpoint-ready plot as pdf
@@ -197,12 +239,6 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
             facing = "outside")))
     )
     
-    ## draw it defining padding distances and annotation/heatmap labels
-    if (!is.null(bigwig.names)) tmp.name <- bigwig.names[p]
-    
-    ## if no names are given use the basename of the bigwig as plot/legend name:
-    if (is.null(bigwig.names)) tmp.name <- tools::file_path_sans_ext(basename(bigwigs[p]))
-    
     GetDate <- function(){ gsub("^20", "", format(Sys.Date(), "%Y%m%d")) }
     
     pdf(paste0(pdf.dir, "/", GetDate(), "_", tmp.name, "_on_", target.name, ".pdf"), width = 2, height = 6)
@@ -216,16 +252,25 @@ plot.EnrHtmp <- function(bigwigs,                     ## a list of bigwig paths 
   } ## end of mclapply plotting function
   ) ## end of mclapply itself
   
+  if (return.roworder) return(tmp.order)
+  
 } ## end wrapper function
 
 ## Example:
-## gr <- makeGRangesFromDataFrame(fread("~/IMTB/Project_Leukemia/ATACseq_May2019/Lists/Clusters/190804_cluster2.bed"), seqnames.field = "V1", start.field = "V2", end.field = "V3")
+require(data.table)
+gr <- makeGRangesFromDataFrame(fread("~/IMTB/Project_Leukemia/ATACseq_May2019/Lists/Clusters/190804_cluster2.bed"), seqnames.field = "V1", start.field = "V2", end.field = "V3")
 
-## plot.EnrHtmp(bigwigs = c("~/IMTB/C1_mean_TMM.bigwig",
-##                          "~/IMTB/C2_TMM.bigwig",
-##                          "~/IMTB/C3_TMM.bigwig",
-##                          "~/IMTB/C4_TMM.bigwig"),
-##             bigwig.names = c("Celltype1", "Celltype2", "Celltype3", "Celltype4"), 
-##             targets.gr = gr, target.name = "cluster2", window.size = 5000, scale.all = TRUE, pdf.dir = "./")
+plot.EnrHtmp(bigwigs = c("~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_LTHSC_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_STHSC_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_MPP_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_CMP_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_GMP_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_Mono_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_GN_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_CLP_TMM.bigwig",
+                         "~/IMTB/Project_Leukemia/Published_Datasets/LaraAstiaso/ChIPseq/H3K4me1/H3K4me1_CD4_TMM.bigwig"),
+                bigwig.names = c("LTHSC", "STHSC", "MPP", "CMP", "GMP", "Mono", "GN", "CLP", "CD4"), legend.title = "H3K4me1 [CPM]",
+                targets.gr = gr, target.name = "cluster2", window.size = 5000, scale.all = TRUE, pdf.dir = "~/Desktop/",
+                reference.bigwig = "STHSC")
 
 
